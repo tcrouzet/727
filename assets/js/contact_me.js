818a249ac59e687c6aad25072b30b31b670df1e2
tcrouzet <tc@tcrouzet.com>
2023-12-05 14:47:05 +0100

    PresqueOK

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -9,11 +9,9 @@ $(function() {
     },
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-	  var url = "https://formspree.io/" + "{% if site.formspree_form_path %}{{ site.formspree_form_path }}{% else %}{{ site.email }}{% endif %}";
+	    var url = formspreeURL;
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -28,7 +26,6 @@ $(function() {
 	dataType: "json",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
@@ -40,7 +37,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>{{ site.data.sitetext[site.locale].contact.success }}</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -52,7 +49,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", {{ site.data.sitetext[site.locale].contact.fail }}"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
